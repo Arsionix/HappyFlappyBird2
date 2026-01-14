37f686a31b92821b6918025135bc9ebf1fc8e8fc
Arsionix <arsi114@mail.ru>
2026-01-14 15:53:21 +0700

    Update Project 26 November

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,31 +1,41 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
-public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+import com.mygdx.game.screens.ScreenGame;
+import com.mygdx.game.screens.ScreenMenu;
+import com.mygdx.game.screens.ScreenRestart;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 1, 0, 1);
-		batch.begin();
-		batch.draw(img, 100, 50);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+public class MyGdxGame extends Game {
+
+    public SpriteBatch batch;
+    public OrthographicCamera camera;
+
+    public static final int SCR_WIDTH = 1280;
+    public static final int SCR_HEIGHT = 720;
+
+    public ScreenGame screenGame;
+    public ScreenMenu screenMenu;
+    public ScreenRestart screenRestart;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, SCR_WIDTH, SCR_HEIGHT);
+
+        screenGame = new ScreenGame(this);
+        screenMenu = new ScreenMenu(this);
+        screenRestart = new ScreenRestart(this);
+
+        setScreen(screenMenu);
+    }
+
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
 }
